15f5f2e2ba88a9756ff200a14c61feca98bdaa16
AlisherB <alisher_b_95@mail.ru>
2018-04-26 15:26:37 +0600

    add HW_SP_19.03.18

---
+++ HW_SP_19.03.18/Sync/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Sync
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.processName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MachineName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Threads = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.workingSet64 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.processName,
+            this.id,
+            this.MachineName,
+            this.workingSet64,
+            this.Threads});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(595, 378);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // processName
+            // 
+            this.processName.Text = "Name";
+            this.processName.Width = 200;
+            // 
+            // id
+            // 
+            this.id.Text = "Id";
+            // 
+            // MachineName
+            // 
+            this.MachineName.Text = "MachineName";
+            this.MachineName.Width = 100;
+            // 
+            // Threads
+            // 
+            this.Threads.DisplayIndex = 3;
+            this.Threads.Text = "Threads";
+            // 
+            // workingSet64
+            // 
+            this.workingSet64.DisplayIndex = 4;
+            this.workingSet64.Text = "Memory";
+            this.workingSet64.Width = 120;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 378);
+            this.Controls.Add(this.listView);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader processName;
+        private System.Windows.Forms.ColumnHeader id;
+        private System.Windows.Forms.ColumnHeader MachineName;
+        private System.Windows.Forms.ColumnHeader Threads;
+        private System.Windows.Forms.ColumnHeader workingSet64;
+    }
+}
+
---
+++ HW_SP_19.03.18/Sync/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Sync
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Thread thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    ShowProcesses();
+                    Thread.Sleep(10000);
+                }
+            });
+            thread.Start();
+
+        }
+        void ShowProcesses()
+        {
+            Process[] processes = Process.GetProcesses();
+            List<ListViewItem> list = new List<ListViewItem>();
+            ListViewItem lvi = null;
+            long memory = 0;
+            try
+            {
+                foreach (Process process in processes)
+                {
+                    memory = process.WorkingSet64 / 1024;
+                    lvi = new ListViewItem(new string[] 
+                    {
+                        process.ProcessName,
+                        process.Id.ToString(),
+                        process.MachineName,
+                        memory.ToString() + " KB",
+                        process.Threads.Count.ToString(),
+                        //process.StartTime.ToString(),
+                    });
+                    list.Add(lvi);
+                }
+            }
+            catch(Win32Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            listView.Invoke(new Action<Object>(x => listView.Items.Clear()), new Object());
+            listView.Invoke(new Action<Object>(x => listView.Items.AddRange(list.ToArray())), new Object());
+           // listView.Invoke(new Action<View>(x => listView.View = x), View.Details);
+        }
+    }
+}
